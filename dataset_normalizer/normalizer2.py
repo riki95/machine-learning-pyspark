2f9b4b6036c15917f24faaac0d5a454da165e97f
Riccardo <riki-95@hotmail.it>
2019-01-11 14:26:47 +0100

    Fixed y

--- dataset_normalizer/normalizer2.py
+++ dataset_normalizer/normalizer2.py
@@ -7,10 +7,15 @@ data = pd.get_dummies(data, columns=['job', 'marital', 'education', 'contact', '
 data.default = data.default.map({'no': 0, 'yes': 1})
 data.housing = data.housing.map({'no': 0, 'yes': 1})
 data.loan = data.loan.map({'no': 0, 'yes': 1})
+data.y = data.y.map({'no': 0, 'yes': 1})
+
 
 # Normalize between 0 and 1
 for c in ['age', 'balance', 'duration', 'pdays', 'campaign', 'previous']:
     data[c] = (data[c] - data[c].min()) / (data[c].max() - data[c].min())
+
+cols_at_end = ['y']
+data = data[[c for c in data if c not in cols_at_end] + [c for c in cols_at_end if c in data]]
 
 data.to_csv('normalized2.csv', sep=',', index=False)
 
